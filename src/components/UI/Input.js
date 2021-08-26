047e34bf6fc1b7c2b74cd4204ceabce45d9b79f5
Igor <ermygut@gmail.com>
2021-08-26 19:01:41 +0300

    add to cart logic via context

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import classes from './Input.module.css';
 
-const Input = props => {
+//with forward-ref we can use it on a custom component
+const Input = React.forwardRef((props, ref) => {
+	//and in args too
 	return (
 		<div className={classes.input}>
 			<label htmlFor={props.input.id}>{props.label}</label>
-			<input {...props.input} />
+			<input ref={ref} {...props.input} /> {/*here ref attribute */}
 		</div>
 	);
-};
+});
 
 export default Input;
