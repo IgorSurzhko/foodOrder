8c55e17fbd3fc95be9469a25341a5ae2f34447d5
Igor <ermygut@gmail.com>
2021-09-09 17:06:18 +0300

    moving from local to firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99
-	}
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map(meal => (
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(`${process.env.REACT_APP_FIRE_BASE}/meals.json`);
+			const responseDate = await response.json();
+
+			const loadedMeals = [];
+			for (const key in responseDate) {
+				loadedMeals.push({
+					id: key,
+					name: responseDate[key].name,
+					description: responseDate[key].description,
+					price: responseDate[key].price
+				});
+			}
+			setMeals(loadedMeals);
+			setIsLoading(false);
+		};
+		fetchMeals();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section className={classes.MealsLoading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+	const mealsList = meals.map(meal => (
 		<MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
 	));
 
