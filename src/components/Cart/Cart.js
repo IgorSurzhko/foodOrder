706c082cb7a6aebaef88f4f70565b45c5313afaa
Igor <ermygut@gmail.com>
2021-09-12 11:09:54 +0300

    post the order to Firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -23,6 +23,16 @@ const Cart = props => {
 		setIsCheckout(true);
 	};
 
+	const submitOrderHandler = userData => {
+		fetch(`${process.env.REACT_APP_FIRE_BASE}/orders.json`, {
+			method: 'POST',
+			body: JSON.stringify({
+				user: userData,
+				orderedItems: cartCtx.items
+			})
+		});
+	};
+
 	const cartItems = (
 		<ul className={classes['cart-items']}>
 			{cartCtx.items.map(item => (
@@ -58,7 +68,7 @@ const Cart = props => {
 				<span>Total Amount</span>
 				<span>{totalAmount}</span>
 			</div>
-			{isCheckout && <Checkout onCancel={props.onHideCart} />}
+			{isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onHideCart} />}
 			{!isCheckout && modalActions}
 		</Modal>
 	);
